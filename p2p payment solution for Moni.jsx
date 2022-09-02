501577f828990d688d6e6d2de349755eba3eab60
Christopher Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2022-09-02 15:54:33 +0100

    Update p2p payment solution for Moni.jsx

--- p2p payment solution for Moni.jsx
+++ p2p payment solution for Moni.jsx
@@ -312,3 +312,44 @@ const updateWallet = async (userId, amount) => {
     console.log(error);
   }
 };
+//...
+
+app.get("/response", async (req, res) => {
+  const { transaction_id } = req.query;
+
+  // URL with transaction ID of which will be used to confirm transaction status
+  const url = `https://api.paystack.com/v3/transactions/${transaction_id}/verify`;
+
+  // Network call to confirm transaction status
+  const response = await axios({
+    url,
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+
+  const { status, currency, id, amount, customer } = response.data.data;
+
+  // check if customer exist in our database
+  const user = await User.findOne({ email: customer.email });
+
+  // check if user have a wallet, else create wallet
+  const wallet = await validateUserWallet(user._id);
+
+  // create wallet transaction
+  await createWalletTransaction(user._id, status, currency, amount);
+
+  // create transaction
+  await createTransaction(user._id, id, status, currency, amount, customer);
+
+  await updateWallet(user._id, amount);
+
+  return res.status(200).json({
+    response: "wallet funded successfully",
+    data: wallet,
+  });
+});
+
+//...
